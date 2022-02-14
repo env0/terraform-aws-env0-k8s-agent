e14585269ae8bc095a3ae9bd5c1eed59744a070b
eladmoshe <elad.moshe@env0.com>
2022-02-14 18:09:57 +0200

    changing aws version

--- examples/all-modules-provisioned-by-user/versions.tf
+++ examples/all-modules-provisioned-by-user/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws        = "~> 3.68.0"
+    aws        = "~> 3.70.0"
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
--- modules/csi-driver/versions.tf
+++ modules/csi-driver/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws        = "~> 3.68.0"
+    aws        = "~> 3.70.0"
     kubernetes = "~> 2.7.0"
     helm       = "~> 2.4.0"
     kubectl = {
